8f227a228c9300dd24955b6481f5671f51d45779
JoFrhwld <JoFrhwld@gmail.com>
2022-11-12 19:14:52 -0500

    writing about

--- src/components/About.js
+++ src/components/About.js
@@ -5,10 +5,164 @@ function About() {
   return (
     <Container>
       <Row className="mt-4 mb-5">
-        <Col md={{ size: 8, offset: 2 }}>
-          <p>This project explores ideas of the residents of Lexington, Kentucky for their city's future through combinations of their own words.</p>
+        <Col md={{ size: 8, offset:2}} style={{fontSize: "1.25em"}}>
+          <h2>
+            What is this?
+          </h2>
+          <p>
+            Lexingtonians submitted over 10,000 comments about their ideas for Lexington's future as part of <a className="text-success" href="https://www.ottlex.org/" target="_blank">On The Table</a>. We wanted to build a tool that explored the sense of what everyone had to say! So we built a language model that cuts up and remixes all of the comments to generate new text.
+          </p>
+
+          <h2>
+            Are these <em>actual</em> comments people left?
+          </h2>
+          <p>
+            You are not very likely to see a whole comment that someone actually submitted, but it's not impossible. In fact, some of the generated comments might express the <em>opposite</em> sentiment of originals. Maybe someone didn't want to cut down all the trees in the city, and said so.
+          </p>
+          <blockquote>
+            <span>original</span>
+            <p>I don't <b>think we should cut down all the trees in the city.</b></p>
+          </blockquote>
+          <p>
+            The model might remix the last part of the comment into something like this:
+          </p>
+          <blockquote>
+            <span>remixed</span>
+            <p>I <b>think we should cut down all the trees in the city.</b></p>
+          </blockquote>
+
+          <p>
+            So always read generated comments with a grain of salt!
+          </p>
+          <h2>How does it work? Is this an AI? 🤖</h2>
+          <p>
+            This isn't an AI. It's model built on the idea that the next word you see in a sentence is predictable given the words that came before. You can probably guess a few words that are likely to fill in the blank.
+          </p>
+          <ul>
+            <li>She went to the baker to get a loaf of ___</li>
+          </ul>
+          <p>
+            The words "baker" and "loaf of" make it more likely that the missing word is "bread" or "sourdough" than "cabbage" or "oranges".
+          </p>
+          <p>
+            To build a the autocomplete model, we split every comment up into its individual words, then counted up how often every word appeared after the two words that came before it. For example, here's how often the following words appeared after "I want"
+          </p>
+          
+          <Row>
+            <Col md={{ size: 2, offset:2}}>
+              <p>"I want..."</p>
+            </Col>
+            <Col md={{ size: 4, offset:0}}>
+            <table>
+              <thead>
+                <tr>
+                  <th>next</th>
+                  <th>frequency</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                 <td>to</td>
+                 <td class="text-end">12</td>
+                </tr>
+                <tr>
+                  <td>all</td>
+                 <td class="text-end">2</td>
+                </tr>
+                <tr>
+                 <td>the</td>
+                 <td class="text-end">2</td>
+                </tr>
+                <tr>
+                 <td>a</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>everybody</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>everyone</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>Lexington</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>more</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>my</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>or</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>police</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>us</td>
+                 <td class="text-end">1</td>
+                </tr>
+                <tr>
+                 <td>you</td>
+                 <td class="text-end">1</td>
+                </tr>
+              </tbody>
+            </table>
+            </Col>
+          </Row>
+
+        <p>The comment generator and autocomplete tools pick the next word, based on these frequencies, meaning it's more likely to pick a word that appeared more often than a word that appeared less often. If this time it picked "all", the process starts all over again with "want all"</p>
+          <Row>
+            <Col md={{ size: 2, offset:2}}>
+              <p>"want all..."</p>
+            </Col>
+            <Col md={{ size: 4, offset:0}}>
+            <table>
+              <thead>
+                <tr>
+                  <th>next &nbsp;&nbsp;</th>
+                  <th>frequency</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                 <td>of</td>
+                 <td class="text-end">2</td>
+                </tr>
+                <tr>
+                  <td>the</td>
+                 <td class="text-end">1</td>
+                </tr>
+              </tbody>
+            </table>
+            </Col>
+          </Row>
+          <p>
+            It's kind of surprising a simple process of picking words like this will generate readable sentences, and it's also why the generated comments sometimes seem like they "lose their train of thought" part way through. They can only remember the previous two words that got generated!
+          </p>
+          <hr></hr>
+          <h2>Technical Details</h2>
+          <p>
+            (Only read on if you care about the technical stuff.)
+          </p>
+          <p>
+            The trigram model was built in R. Qualitative comments were tokenized and named entitites identified via the <a className="text-success" href="https://cran.r-project.org/web/packages/spacyr/" target="_blank">spacyR package</a>, using the <a className="text-success" href="https://spacy.io/models/en#en_core_web_trf" target="_blank">en_core_web_trf model</a>. Some light by-hand text normalization was done to, e.g. expand abbreviations for "St." and "Rd." to "Street" and "Road", as well as trying to normalize all spelling variants of "Lextran". Identified named entities were title cased, the pronoun "I" was upper-cased, and all other tokens lower-cased. A number of punctuation and contraction tokens had their left or right "lean" hand coded for detokenization. The resulting model was written to json for processing by the javascript comment generators.
+          </p>
+          <p>
+            The site is built in React and hosted on firebase.
+          </p>
+
+          {/* <p>This project explores ideas of the residents of Lexington, Kentucky for their city's future through 
+          combinations of their own words.</p>
           <p>The words in these automatically-generated comments are selected by a <a className="text-success" href="https://en.wikipedia.org/wiki/Trigram" target="_blank">trigram model</a>. This model was trained on <a className="text-success" href="https://static1.squarespace.com/static/61819f627baf68009186fb50/t/62b236b49282122e7044db59/1655846584979/OTT+Posters+for+Website.pdf" target="_blank">a data set</a> that was gathered by <a className="text-success" href="https://www.civiclex.org/" target="_blank">Civic Lex</a> as part of their 2022 <a className="text-success" href="https://www.ottlex.org/" target="_blank">On The Table</a> program.</p>
-          <p>Output from trigram models doesn't always make sense, but it gives you a glimpse into the data it's trained on.</p>
+          <p>Output from trigram models doesn't always make sense, but it gives you a glimpse into the data it's trained on.</p> */}
         </Col>
       </Row>
     </Container>
